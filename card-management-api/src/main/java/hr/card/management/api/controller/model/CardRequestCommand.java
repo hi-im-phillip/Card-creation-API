0b8068a8b09bc1a80705608273448b5a05265d6e
filip <fkasum1@vvg.hr>
2024-04-04 18:01:36 +0200

    move jakarta validation on api layer

--- card-management-api/src/main/java/hr/card/management/api/controller/model/CardRequestCommand.java
+++ card-management-api/src/main/java/hr/card/management/api/controller/model/CardRequestCommand.java
@@ -1,6 +1,8 @@
 package hr.card.management.api.controller.model;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,8 +16,15 @@ import lombok.NoArgsConstructor;
 public class CardRequestCommand {
 
     private Long id;
+    @NotBlank(message = "First name cannot be empty")
+    @Pattern(regexp = "[A-Za-z-']*", message = "First name contains illegal characters")
     private String firstName;
+    @NotBlank(message = "Last name cannot be empty")
+    @Pattern(regexp = "[A-Za-z-']*", message = "Last name contains illegal characters")
     private String lastName;
+    @NotBlank(message = "Last name cannot be empty")
     private String status;
+    @NotBlank(message = "OIB cannot be empty")
+    @Pattern(regexp = "\\b(?:\\d{11}|HR\\d{11})\\b", message = "OIB contains illegal characters")
     private String oib;
 }
--- card-management-api/src/main/java/hr/card/management/api/infrastructure/model/CardRequest.java
+++ card-management-api/src/main/java/hr/card/management/api/infrastructure/model/CardRequest.java
@@ -1,8 +1,6 @@
 package hr.card.management.api.infrastructure.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Entity
@@ -18,16 +16,9 @@ public class CardRequest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "First name cannot be empty")
-    @Pattern(regexp = "[A-Za-z-']*", message = "First name contains illegal characters")
     private String firstName;
-    @NotBlank(message = "Last name cannot be empty")
-    @Pattern(regexp = "[A-Za-z-']*", message = "Last name contains illegal characters")
     private String lastName;
     private String status;
-
-    @NotBlank(message = "OIB cannot be empty")
-    @Pattern(regexp = "\\b(?:\\d{11}|HR\\d{11})\\b", message = "OIB contains illegal characters")
     @Column(unique = true)
     private String oib;
 }
